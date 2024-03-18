819fae8baad6fc396cf96896cee106218f0af828
lby-rgyyt <m13051662977@gmail.com>
2024-03-18 12:38:18 -0700

    bugs

--- src/components/CreditsPage.jsx
+++ src/components/CreditsPage.jsx
@@ -1,11 +1,30 @@
-// CreditsPage.jsx
 import React from 'react';
+import "../style/CreditsPage.css"
 
 const CreditsPage = () => {
   return (
-    <div>
+    <div className="credits-container">
       <h1>Credits</h1>
-      <p>This page lists the creator and provides a link to their GitHub repo.</p>
+      <div className="links">
+        <p>
+          Our Websites: 
+          <p></p>
+          <a href="https://lby-rgyyt.github.io/" className="link" target="_blank" rel="noopener noreferrer">
+            lby-rgyyt.github.io
+          </a>
+          <p></p>
+          <a href="https://kunwang0527.github.io/Personal-website/" className="link" target="_blank" rel="noopener noreferrer">
+          kunwang0527.github.io/Personal-website/
+          </a>
+        </p>
+        <p>
+          GitHub Repo: 
+          <p></p>
+          <a href="https://github.com/lby-rgyyt/5610-poj2" className="link" target="_blank" rel="noopener noreferrer">
+            lby-rgyyt/5610-poj2
+          </a>
+        </p>
+      </div>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-
 import {
   createBrowserRouter,
   RouterProvider,
   Route,
 } from "react-router-dom";
+
+
 import { AppProvider } from './components/AppContext';
 import NavBar from './components/NavBar';
 import HomePage from './components/HomePage';
